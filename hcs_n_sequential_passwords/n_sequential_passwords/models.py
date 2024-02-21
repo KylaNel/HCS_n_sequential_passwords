62837a14f8c983ce9d0c4b5642ed6fd526bf5ebb
Kyla <2576953n@student.gla.ac.uk>
2024-02-21 19:19:47 +0000

    created user model

--- hcs_n_sequential_passwords/n_sequential_passwords/models.py
+++ hcs_n_sequential_passwords/n_sequential_passwords/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    username = models.TextField(blank=True)
+    pattern_lock = models.JSONField(blank=True)
+    pin = models.PositiveSmallIntegerField(blank=True)
+    password = models.CharField(blank=True)
+
+    def __str__(self):
+        return self.username

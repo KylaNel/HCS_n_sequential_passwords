47a28c7ec454273788bd078c4dce2f562081665d
Kyla <2576953n@student.gla.ac.uk>
2024-02-22 11:22:43 +0000

    Fixed password field max_length requirement

--- hcs_n_sequential_passwords/n_sequential_passwords/models.py
+++ hcs_n_sequential_passwords/n_sequential_passwords/models.py
@@ -5,7 +5,7 @@ class User(models.Model):
     username = models.TextField(blank=True)
     pattern_lock = models.JSONField(blank=True)
     pin = models.PositiveSmallIntegerField(blank=True)
-    password = models.CharField(blank=True)
+    password = models.CharField(blank=True, max_length=10)
 
     def __str__(self):
         return self.username
